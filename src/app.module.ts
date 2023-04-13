c4ea9c054dbb2b1b801b0870815c33ff940e552b
ballde <67853463+YuSunjo@users.noreply.github.com>
2023-04-13 11:47:57 +0900

    feat(article): 프리티어 정리 적용 및 env 파일 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,33 +2,37 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
 import { MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { CorsMiddleware } from './middlewares/cors.middleware';
-// import { User } from './users/user.entity';
-// import { UsersModule } from './users/users.module';
-import { Article } from './articles/article.entity';
 import { ArticlesModule } from './articles/articles.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forRoot({
-        type: 'mysql',
-        host: 'db',
-        port: 3306,
-        username: 'root',
-        password: 'docker123',
-        database: 'nest',
-        entities: [Article],
-        synchronize: true,
-    }), ArticlesModule],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ArticlesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 // export class AppModule {
 //     constructor(private dataSource: DataSource) { }
 // }
 export class AppModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
-        consumer.apply(CorsMiddleware).forRoutes('*');
-    }
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorsMiddleware).forRoutes('*');
+  }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -5,24 +5,24 @@ import { Article } from './article.entity';
 
 @Injectable()
 export class ArticlesService {
-    constructor(
-        @InjectRepository(Article)
-        private ArticlesRepository: Repository<Article>,
-    ) { }
+  constructor(
+    @InjectRepository(Article)
+    private ArticlesRepository: Repository<Article>,
+  ) {}
 
-    findAll(): Promise<Article[]> {
-        return this.ArticlesRepository.find();
-    }
+  findAll(): Promise<Article[]> {
+    return this.ArticlesRepository.find();
+  }
 
-    findOne(postId: number): Promise<Article | null> {
-        return this.ArticlesRepository.findOneBy({ postId });
-    }
+  findOne(postId: number): Promise<Article | null> {
+    return this.ArticlesRepository.findOneBy({ postId });
+  }
 
-    async remove(id: number): Promise<void> {
-        await this.ArticlesRepository.delete(id);
-    }
+  async remove(id: number): Promise<void> {
+    await this.ArticlesRepository.delete(id);
+  }
 
-    async create(Article: Article): Promise<Article> {
-        return this.ArticlesRepository.save(Article);
-    }
+  async create(Article: Article): Promise<Article> {
+    return this.ArticlesRepository.save(Article);
+  }
 }
