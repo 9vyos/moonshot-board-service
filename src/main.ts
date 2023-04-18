4de553c35871d018b027ddce725d3f8e8ef8506d
ballde <67853463+YuSunjo@users.noreply.github.com>
2023-04-18 10:28:42 +0900

    feat(article): transactional 적용중 initializeTransactionalContext 추가

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 
 async function bootstrap() {
+  initializeTransactionalContext();
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: true,
   });
