83a92eede96c62e8e887b08be89a407aa5c3e68a
ballde <67853463+YuSunjo@users.noreply.github.com>
2023-04-19 11:32:39 +0900

    feat(refactoring): resolver 안올린거 올립니다..

---
+++ src/articles/articles.resolver.ts
@@ -0,0 +1,36 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { ArticlesService } from './articles.service';
+import { CreateBoardRequest } from './dto/request/create.board.request';
+import { BoardInfoResponse } from './dto/response/board.info.response';
+import { PostCategoryInfoResponse } from './dto/response/post.category.info.response';
+import { CreatePostCategoryRequest } from './dto/request/create.post.category.request';
+
+@Resolver()
+export class ArticlesResolver {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Query(() => String)
+  articleHello() {
+    return 'Hello from article resolver';
+  }
+
+  @Mutation(() => BoardInfoResponse)
+  async createBoard(@Args('request') request: CreateBoardRequest) {
+    return await this.articlesService.createBoard(request);
+  }
+
+  @Query(() => [BoardInfoResponse])
+  async retrieveBoard() {
+    return await this.articlesService.retrieveBoard();
+  }
+
+  @Mutation(() => PostCategoryInfoResponse)
+  async createCategory(@Args('request') request: CreatePostCategoryRequest) {
+    return await this.articlesService.createCategory(request);
+  }
+
+  @Query(() => [PostCategoryInfoResponse])
+  async retrieveCategory() {
+    return await this.articlesService.retrieveCategory();
+  }
+}
